53e1cb611097406e5001a7e810b660f2f20c8f77
IAmMinhAnh <nguyendangminhanh8@gmail.com>
2026-01-06 16:17:50 -0500

    C: Draw Triangle in the middle

--- dev_test/main.py
+++ dev_test/main.py
@@ -1,12 +1,15 @@
 import pygame 
 from logger import log_state
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from player import Player
 
 def main():
     pygame.init()
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         log_state()
@@ -16,6 +19,7 @@ def main():
                 return 
 
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
