f094f5f8c396c71a9d33d935881feca92c85937e
IAmMinhAnh <nguyendangminhanh8@gmail.com>
2026-01-07 09:37:15 -0500

    D: Update player moverment and Create Asteroids

--- dev_test/main.py
+++ dev_test/main.py
@@ -2,6 +2,8 @@ import pygame
 from logger import log_state
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
@@ -9,17 +11,30 @@ def main():
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
+    ast = AsteroidField()
+
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         log_state()
-
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return 
 
         screen.fill("black")
-        player.draw(screen)
+
+        updatable.update(dt)
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
