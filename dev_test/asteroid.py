c6d441b0f9be38d49c4334d07db3963e957038ee
IAmMinhAnh <nguyendangminhanh8@gmail.com>
2026-01-07 16:12:44 -0500

    G: Update Asteroid class so that it can kill itself and create two new smaller asteroids

--- dev_test/asteroid.py
+++ dev_test/asteroid.py
@@ -1,10 +1,27 @@
 import pygame 
+import random 
 from circleshape import CircleShape
-from constants import LINE_WIDTH
+from constants import LINE_WIDTH, ASTEROID_MIN_RADIUS
+from logger import log_event
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
         super().__init__(x, y, radius)
+
+    def split(self):
+        self.kill()
+        if (self.radius <= ASTEROID_MIN_RADIUS):
+            return 
+        else:
+            log_event("asteroid_split")
+            angle = random.uniform(20, 50)
+            direc1 = self.velocity.rotate(angle)
+            direc2 = self.velocity.rotate(-angle)
+            new_radius = self.radius - ASTEROID_MIN_RADIUS
+            asteroid1 = Asteroid(self.position.x, self.position.y, new_radius)
+            asteroid2 = Asteroid(self.position.x, self.position.y, new_radius)
+            asteroid1.velocity = direc1 * 1.2
+            asteroid2.velocity = direc2 * 1.2
 
     def draw(self, screen):
         pygame.draw.circle(screen, "white", self.position, self.radius, LINE_WIDTH)
